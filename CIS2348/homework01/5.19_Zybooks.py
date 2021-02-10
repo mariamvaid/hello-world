70bb7378c87eafe03ce78e98ca138d290551225a
mariamvaid <77936989+mariamvaid@users.noreply.github.com>
2021-02-10 01:22:29 -0600

    Create 5.19_Zybooks.py

---
+++ CIS2348/homework01/5.19_Zybooks.py
@@ -0,0 +1,10 @@
+#Mariam Vaid
+#1477614
+print("Davy's auto shop services")
+
+serve = ["Oil Change", "Tire Rotation", "Car Wash", "Car Wax"]
+
+price = [ 35, 19, 7, 12]
+
+for i in range(len(serve)):
+    print(serve[i], '--', price[i])
